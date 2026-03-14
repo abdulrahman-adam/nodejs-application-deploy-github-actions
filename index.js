279edfeb460d5b255d2157efe408a80c750e8595
Abdulrahman <abdulrahman939291@gmail.com>
2026-03-14 17:09:06 +0100

    deploy the server after updating

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8089
 
 app.get('/', (req, res) => {
-    return res.json({msg: 'Hello from the from the server'})
+    return res.json({msg: 'Hello from the from the server v2 deploy\n'})
 })
 
 app.listen(PORT, () => {
